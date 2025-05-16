03c3258499a0022719fecea5fa166fa5b86e2da9
Pratham Gautam <mehul.gautam13@gmail.com>
2025-05-16 14:10:02 +0530

    auth-login

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-# File: backend/app.py
-from flask import Flask
+from flask import Flask, request
 from routes.data_entry import data_entry_bp
 from routes.dashboard import dashboard_bp
+from routes.auth_routes import auth_routes_bp
 from auth import require_auth
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-# Middleware for authentication (placeholder)
+@app.route('/')
+def home():#
+    return "Backend server is running"#
+
 @app.before_request
 def check_auth():
-    require_auth()
+    if request.path != '/api/login':
+        require_auth()
 
 # Register blueprints
 app.register_blueprint(data_entry_bp, url_prefix='/api/form')
 app.register_blueprint(dashboard_bp, url_prefix='/api/dashboard')
+app.register_blueprint(auth_routes_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
